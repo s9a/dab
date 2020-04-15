a8d28f186702c5601abe0c1d97265bec19216081
Ryan Van Etten <ryanve@gmail.com>
2020-04-15 08:09:39 -0400

    Eureka dab-png

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# dab-png
+# `dab-png`
+
+## install
+
+```bash
+npm install dab-png --save
+```
+
+## usage
+
+```js
+const dabPng = require("dab-png")
+```
+
+```js
+dabPng(deets, callback=saved)
+```
+
+### `deets`
+
+```js
+dabPng({
+  fill: "#bae",
+  file = "bae.png",
+  width = 1600,
+  height = 900,
+})
+```
+
+- `fill` with any recognizable color format. Default: `"transparent"`
+- `width` in pixels. Default: `height || 1`
+- `height` in pixels. Default: `width || 1`
+- `file` is the filename or filepath to save to. Default: <code><var>width</var><b>x</b><var>height</var><b>.png</b></code>
+
+### `callback`
+
+- default `callback` logs success or throws error
+- you may override with your own `callback`
+
+```js
+dabPng({}, (err, info) => {
+  if (err) throw err
+  console.log(info)
+})
+```
+
+## CLI
+
+- Deets can be in any order
+- Supports hex colors and X11 names
+- Dimension syntax is <code><var>width</var><b>x</b><var>height</var></code> or just `width` for square
+- Quote or escape as needed
+  * Good: `"#000"` `\#000` `"black"` `black`
+  * Fails: `#000`
+
+
+### cli examples
+
+```
+dab-png "#dab" dab.png 1280
+dab-png "#dab" dab.png 1280x640
+dab-png "Lime" 1280
+dab-png "Lime" 1280x640
+dab-png "Lime" 1280x640 lime.png
+```
---
+++ test.js
@@ -0,0 +1,91 @@
+const dabPng = require("./")
+const wtf = require("./wtf")
+const fs = require("fs")
+const test = require("tape")
+const cleanup = []
+
+test("ufo", t => {
+  t.plan(1)
+  t.throws(() => dabPng())
+})
+
+test("defaults", t => {
+  const fill = "#dab"
+  const file = "test_defaults.png"
+  t.plan(5)
+  dabPng({ fill, file }, (err, info) => {
+    if (err) throw err
+    t.ok(info)
+    t.equal(fs.existsSync(file), true)
+    t.equal(info.format, "png")
+    t.equal(info.width, 1)
+    t.equal(info.height, 1)
+    cleanup.push(file)
+  })
+})
+
+test("portrait", t => {
+  const fill = "#bae"
+  const file = "test_landscape.png"
+  const width = 600
+  const height = 900
+  t.plan(5)
+  dabPng({ fill, file, width, height }, (err, info) => {
+    if (err) throw err
+    t.ok(info)
+    t.equal(fs.existsSync(file), true)
+    t.equal(info.format, "png")
+    t.equal(info.width, width)
+    t.equal(info.height, height)
+    cleanup.push(file)
+  })
+})
+
+test("autoheight", t => {
+  const fill = "#bff"
+  const file = "autoheight.png"
+  const width = 500
+  t.plan(5)
+  dabPng({ fill, file, width }, (err, info) => {
+    if (err) throw err
+    t.ok(info)
+    t.equal(fs.existsSync(file), true)
+    t.equal(info.format, "png")
+    t.equal(info.width, width)
+    t.equal(info.height, width)
+    cleanup.push(file)
+  })
+})
+
+test("autowidth", t => {
+  const fill = "#bed"
+  const file = "test_autowidth.png"
+  const height = 500
+  t.plan(5)
+  dabPng({ fill, file, height }, (err, info) => {
+    if (err) throw err
+    t.ok(info)
+    t.equal(fs.existsSync(file), true)
+    t.equal(info.format, "png")
+    t.equal(info.width, height)
+    t.equal(info.height, height)
+    cleanup.push(file)
+  })
+})
+
+test("wtf", t => {
+  const empty = {}
+  const empties = ["", [], {}, null, undefined]
+
+  t.plan(empties.length + 4)
+  empties.forEach(v => t.deepEqual(wtf(v), empty))
+
+  t.deepEqual(wtf("#333"), { fill: "#333" })
+  t.deepEqual(wtf("333"), { width: 333, height: 333 })
+  t.deepEqual(wtf("any.png"), { file: "any.png" })
+  t.deepEqual(wtf("80x20"), { width: 80, height: 20 })
+})
+
+test.onFinish(() => {
+  cleanup.forEach(file => fs.unlinkSync(file))
+})
